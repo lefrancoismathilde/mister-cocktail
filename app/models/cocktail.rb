1399e6156a57089b3a753d6a7a40a91f4f10c17c
Mathilde Lefrancois <mathilde@kwaala.fr>
2015-08-21 14:53:33 +0200

    before amazon

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,6 @@
 class Cocktail < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
+  validates :picture, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
