246a0c45268582fbe93f44b7a69bfbce7ff4e393
Mathilde Lefrancois <mathilde@kwaala.fr>
2015-08-21 17:43:48 +0200

    fuck 2

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,11 +1,14 @@
 class Cocktail < ActiveRecord::Base
-  validates :name, uniqueness: true, presence: true
-  validates :picture, presence: true
-  has_many :doses, dependent: :destroy
-  has_many :ingredients, through: :doses
-  has_attached_file :picture,
+
+ has_attached_file :picture,
     styles: { medium: "300x300>", thumb: "100x100>" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
+
+  validates :name, uniqueness: true, presence: true
+  validates :picture, presence: true
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+Dose.destroy_all
 Cocktail.destroy_all
 Ingredient.destroy_all
 
